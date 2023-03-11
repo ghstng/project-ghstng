628f1985980fc9b5f0f0b71ea7b8bf2c9c204875
ghstng <garden.greg@gmail.com>
2023-03-11 13:29:41 -0600

    adding partytown and running gtm in diff thread

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,15 @@ import yaml from '@rollup/plugin-yaml';
 import sitemap from '@astrojs/sitemap';
 import prefetch from "@astrojs/prefetch";
 import remarkBreaks from 'remark-breaks';
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://ghstng.com',
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkBreaks],
-    }), 
-    sitemap(), 
-    prefetch(), 
-  ],
+  integrations: [mdx({
+    remarkPlugins: [remarkBreaks]
+  }), sitemap(), prefetch(), partytown()],
   vite: {
-    plugins: [
-      yaml()
-    ],
-  },
+    plugins: [yaml()]
+  }
 });
